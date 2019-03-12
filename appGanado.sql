128ffe3d26fb587bb0d9b35d0c4160cde36e40a7
Javier Ardila <32044695+renderjaviii@users.noreply.github.com>
2019-03-12 16:43:01 -0500

    Update appGanado.sql

--- appGanado.sql
+++ appGanado.sql
@@ -1,20 +1,20 @@
 create table Lote(
-	id integer,
+	id serial,
 	nombre varchar(255),
 	primary key(id)
 );
 create table Raza(
-	id integer,
+	id serial,
 	nombre varchar(255),
 	primary key(id)
 );
 create table Proposito(
-	id integer,
+	id serial,
 	nombre varchar(255),
 	primary key(id)
 );
 create table Genero(
-	id integer,
+	id serial,
 	nombre varchar(255),
 	primary key(id)
 );
@@ -22,10 +22,10 @@ create table Genero(
 create table Ganado(
 	id integer,
 	nombre varchar(255),
-	id_Lote integer REFERENCES Lote(id) ON DELETE RESTRICT,
-	id_Raza integer REFERENCES Raza(id) ON DELETE RESTRICT,
-	id_Proposito integer REFERENCES Proposito(id) ON DELETE RESTRICT,
-	id_Genero integer REFERENCES Genero(id) ON DELETE RESTRICT, 
+	id_Lote integer REFERENCES Lote(id) ON DELETE CASCADE,
+	id_Raza integer REFERENCES Raza(id) ON DELETE CASCADE,
+	id_Proposito integer REFERENCES Proposito(id) ON DELETE CASCADE,
+	id_Genero integer REFERENCES Genero(id) ON DELETE CASCADE, 
 	edad integer,
 	peso float,
 	foto varchar(255),
@@ -35,29 +35,29 @@ create table Ganado(
 
 
 create table Finca(
-	id integer,
+	id serial,
 	nombre varchar(255),
 	ubicacion varchar(255),
-	foto varchar(255),
-	area varchar(255),
+	foto varchar,
+	area float,
 	primary key(id)
 );
 create table Finca_Ganado(
-	id_Ganado integer REFERENCES Ganado(id) ON DELETE RESTRICT,
-	id_Finca integer REFERENCES Finca(id) ON DELETE RESTRICT,
+	id_Ganado integer REFERENCES Ganado(id) ON DELETE CASCADE,
+	id_Finca integer REFERENCES Finca(id) ON DELETE CASCADE,
 	primary key(id_Ganado, id_Finca)
 );
 
 
 create table Tipo_Evento(
-	id integer,
+	id serial,
 	nombre varchar(255),
 	primary key(id)
 );
 create table Evento(
-	id_Finca integer REFERENCES Finca(id) ON DELETE RESTRICT,
 	fecha date,
-	id_Tipo_Evento integer REFERENCES Tipo_Evento(id) ON DELETE RESTRICT,
+	id_Finca integer REFERENCES Finca(id) ON DELETE CASCADE,
+	id_Tipo_Evento integer REFERENCES Tipo_Evento(id) ON DELETE CASCADE,
 	nombre varchar(255),
 	detalles varchar(255),
 	encargado varchar(255),
@@ -65,56 +65,56 @@ create table Evento(
 );
 
 create table Rol(
-	id integer,
-	nombre integer,
+	id serial,
+	nombre varchar(255),
 	primary key(id)
 );
 create table Usuario(
 	telefono integer,
+	id_Rol integer REFERENCES Rol(id) ON DELETE CASCADE,
 	nombre varchar(255),
 	correo varchar(255),
-	id_Rol integer REFERENCES Rol(id) ON DELETE RESTRICT,
 	foto varchar(255),
 	primary key(Telefono)
 );
 
 create table Finca_Usuario(
-	telefono_Usuario integer REFERENCES Usuario(telefono) ON DELETE RESTRICT,
-	id_Finca integer REFERENCES Finca(id) ON DELETE RESTRICT,
+	telefono_Usuario integer REFERENCES Usuario(telefono) ON DELETE CASCADE,
+	id_Finca integer REFERENCES Finca(id) ON DELETE CASCADE,
 	primary key(telefono_Usuario, id_Finca)
 );
 
 
 create table Backup(
-	id integer,
+	id serial,
 	fecha date,
-	telefono_Usuario integer REFERENCES Usuario(telefono) ON DELETE RESTRICT,
+	telefono_Usuario integer REFERENCES Usuario(telefono) ON DELETE CASCADE,
 	primary key(id)
 );
 
 create table Tanque(
-	id integer,
+	id serial,
 	nombre varchar(255),
 	capacidad float,
 	primary key(id)
 );
 create table Finca_Tanque(
-	id_Finca integer REFERENCES Finca(id) ON DELETE RESTRICT,
-	id_Tanque integer REFERENCES Tanque(id) ON DELETE RESTRICT,
+	id_Finca integer REFERENCES Finca(id) ON DELETE CASCADE,
+	id_Tanque integer REFERENCES Tanque(id) ON DELETE CASCADE,
 	fecha date,
 	produccion float,
-	primary key(id_Finca, id_Tanque)
+	primary key(id_Finca, id_Tanque, fecha)
 );
 
 create table Tipo_Historia(
-	id integer,
-	telefono_Usuario integer REFERENCES Usuario(telefono) ON DELETE RESTRICT,
+	id serial,
+	telefono_Usuario integer REFERENCES Usuario(telefono) ON DELETE CASCADE,
 	nombre varchar(255),
 	primary key(id, telefono_Usuario)
 );
 create table Cuaderno_Historias_Ganado(
-	id integer,
-	id_Ganado integer REFERENCES Ganado(id) ON DELETE RESTRICT,
+	id serial,
+	id_Ganado integer REFERENCES Ganado(id) ON DELETE CASCADE,
 	nombre_Historia varchar(255),
 	fecha date,
 	detalles varchar(255),
