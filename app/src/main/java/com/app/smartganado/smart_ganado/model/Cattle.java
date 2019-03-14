2156021c91f87a7a6cb2ccb227ab3f083d1413f5
CristianLeal22 <41762744+CristianLeal22@users.noreply.github.com>
2019-03-14 00:43:38 -0500

    Informacion, de ganado, login y Registro

--- app/src/main/java/com/app/smartganado/smart_ganado/model/Cattle.java
+++ app/src/main/java/com/app/smartganado/smart_ganado/model/Cattle.java
@@ -13,7 +13,7 @@ public class Cattle {
     private String foto;
     private String detalles;
 
-    public Cattle(int id, String nombre, int id_Lote, int id_Raza, int id_Proposito, int id_Genero, int edad, double peso, String foto, String detalles) {
+    public Cattle(int id, String nombre, int id_Lote, int id_Raza, int id_Proposito, int id_Genero, int edad, double peso, String foto, String detalles ) {
         this.id = id;
         this.nombre = nombre;
         this.id_Lote = id_Lote;
@@ -23,6 +23,89 @@ public class Cattle {
         this.edad = edad;
         this.peso = peso;
         this.foto = foto;
+        this.detalles = detalles;
+    }
+
+    public Cattle() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId_Lote() {
+        return id_Lote;
+    }
+
+    public void setId_Lote(int id_Lote) {
+        this.id_Lote = id_Lote;
+    }
+
+    public int getId_Raza() {
+        return id_Raza;
+    }
+
+    public void setId_Raza(int id_Raza) {
+        this.id_Raza = id_Raza;
+    }
+
+    public int getId_Proposito() {
+        return id_Proposito;
+    }
+
+    public void setId_Proposito(int id_Proposito) {
+        this.id_Proposito = id_Proposito;
+    }
+
+    public int getId_Genero() {
+        return id_Genero;
+    }
+
+    public void setId_Genero(int id_Genero) {
+        this.id_Genero = id_Genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(String detalles) {
         this.detalles = detalles;
     }
 
