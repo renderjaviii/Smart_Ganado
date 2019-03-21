154c339b3400902d179ec58cc20258189d01612a
CristianLeal22 <41762744+CristianLeal22@users.noreply.github.com>
2019-03-20 22:42:17 -0500

    Informacion, de ganado, login y Registro

--- app/src/main/java/com/app/smartganado/smart_ganado/view/NewCattleActivity.java
+++ app/src/main/java/com/app/smartganado/smart_ganado/view/NewCattleActivity.java
@@ -20,25 +20,49 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class NewCattleActivity extends AppCompatActivity {
-    Spinner opciones, Raza, Proposito, Genero;
+    Spinner Finca,Lote, Raza, Proposito, Genero;
     EditText TXTcodigo, TXTedad, TXTPeso;
     Cattle cattle;
     public APIService myApiService;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cattle = new Cattle();
         setContentView(R.layout.activity_new_cattle);
 
-        //Spinner Clasificacion, Raza, Proposito, Genero
-        opciones = (Spinner) findViewById(R.id.Clasificacion);
+        //Spinner Finca,Clasificacion, Raza, Proposito, Genero
+        Lote = (Spinner) findViewById(R.id.Clasificacion);
         Raza = (Spinner) findViewById(R.id.RazaTexto);
         Proposito = (Spinner) findViewById(R.id.Proposito);
         Genero = (Spinner) findViewById(R.id.Genero);
+        Finca = (Spinner) findViewById(R.id.Finca);
+        TXTcodigo = (EditText)findViewById(R.id.Codigo);
+        TXTedad = (EditText)findViewById(R.id.EdadGanado);
+        TXTPeso = (EditText)findViewById(R.id.Peso);
+        if(getIntent().getSerializableExtra("Info")==null){
+            cattle = new Cattle();
+        }else{
+            Cattle item = (Cattle) getIntent().getSerializableExtra("Info");
+            TXTcodigo.setText(item.getId()+"");
+            TXTedad.setText(item.getEdad()+"");
+            TXTPeso.setText(item.getPeso()+"");
+
+
+            if (item.getId_Genero()==1){
+                String inicializarItem = "Macho";
+                Genero.setSelection(obtenerPosicionItem(Genero, inicializarItem));    Genero.setEnabled(false);
+            }else{
+                String inicializarItem = "Hembra";
+                Genero.setSelection(obtenerPosicionItem(Genero, inicializarItem));
+                Genero.setEnabled(false);
+            }
+            TXTcodigo.setEnabled(false);
+            TXTedad.setEnabled(false);
+            TXTPeso.setEnabled(false);
+        }
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.opciones, android.R.layout.simple_spinner_item);
-        opciones.setAdapter(adapter);
+        Lote.setAdapter(adapter);
 
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Raza, android.R.layout.simple_spinner_item);
         Raza.setAdapter(adapter2);
@@ -50,19 +74,8 @@ public class NewCattleActivity extends AppCompatActivity {
         Genero.setAdapter(adapter4);
 
         ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this, R.array.Finca, android.R.layout.simple_spinner_item);
-        Genero.setAdapter(adapter5);
+        Finca.setAdapter(adapter5);
 
-
-        //TXT Codigo, edad, Peso
-        TXTcodigo = (EditText) findViewById(R.id.Codigo);
-        TXTedad = (EditText) findViewById(R.id.EdadGanado);
-        TXTPeso = (EditText) findViewById(R.id.Peso);
-
-        cattle.setId(Integer.parseInt(TXTcodigo.toString()));
-        cattle.setEdad(Integer.parseInt(TXTedad.toString()));
-        cattle.setEdad(Integer.parseInt(TXTPeso.toString()));
-
-        Cattle cattle = new Cattle(1, "Vaca leal", 1, 1, 1, 1, 1, 1, "url", "");
         //insert a new cattle
         if (myApiService == null)
             myApiService = APIUtils.getAPIService();
@@ -82,23 +95,47 @@ public class NewCattleActivity extends AppCompatActivity {
 
     }
 
-    //Metodo para el mensaje del ButtomImage
-    public void MensajePrueba(View view) {
-        Toast.makeText(this, "Cambiar imagen", Toast.LENGTH_SHORT).show();
-    }
-
     //Metodo para la validacion de los campos
     public void Registro(View view) {
-        if (TXTcodigo.getText().toString().isEmpty() || TXTedad.getText().toString().isEmpty() || TXTPeso.getText().toString().isEmpty()) {
+        if (TXTcodigo.getText().toString().isEmpty() || TXTedad.getText().toString().isEmpty() || TXTPeso.getText().toString().isEmpty()==true) {
             Toast.makeText(this, "Debes ingresar todos los datos", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Registrado", Toast.LENGTH_SHORT).show();
+            Extraccion();
+            //Entrar a vista ver Ganado
+            Intent i = new Intent(this, ViewCattleActivity.class);
+            startActivity(i);
         }
-
-
-        //Entrar a vista ver Ganado
-        Intent i = new Intent(this, ViewCattleActivity.class);
-        startActivity(i);
     }
+
+    public Cattle Extraccion (){
+        Cattle nuevo = new Cattle();
+        nuevo.setEdad(R.id.EdadGanado);
+        nuevo.setId_Lote(R.id.Clasificacion);
+        nuevo.setId_Raza(R.id.RazaTexto);
+        nuevo.setId_Proposito(R.id.Proposito);
+        nuevo.setId_Genero(R.id.Genero);
+        nuevo.setEdad(R.id.EdadGanado);
+        nuevo.setPeso(R.id.Peso);
+        System.out.println(nuevo.toString());
+        return nuevo;
+    }
+
+    public static int obtenerPosicionItem(Spinner spinner, String fruta) {
+        //Creamos la variable posicion y lo inicializamos en 0
+        int posicion = 0;
+        //Recorre el spinner en busca del ítem que coincida con el parametro `String fruta`
+        //que lo pasaremos posteriormente
+        for (int i = 0; i < spinner.getCount(); i++) {
+            //Almacena la posición del ítem que coincida con la búsqueda
+            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(fruta)) {
+                posicion = i;
+            }
+        }
+        //Devuelve un valor entero (si encontro una coincidencia devuelve la
+        // posición 0 o N, de lo contrario devuelve 0 = posición inicial)
+        return posicion;
+    }
+
 
 }
